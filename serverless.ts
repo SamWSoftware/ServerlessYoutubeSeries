99cf2c45118f92572107a85242b239635ad3aa92
Sam Williams <sam@completecoding.io>
2021-11-20 16:02:04 +0100

    feat(start): starting format

--- serverless.ts
+++ serverless.ts
@@ -1,39 +1,39 @@
 import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  service: 'serverlessyoutubeseries',
-  frameworkVersion: '2',
-  custom: {
-    webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
-  },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
+    service: 'serverlessyoutubeseries',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true,
+        },
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    // Add the serverless-webpack plugin
+    plugins: ['serverless-webpack'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs12.x',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        },
     },
-  },
-  functions: {
-    hello: {
-      handler: 'handler.hello',
-      events: [
-        {
-          http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+    functions: {
+        hello: {
+            handler: 'handler.hello',
+            events: [
+                {
+                    http: {
+                        method: 'get',
+                        path: 'hello',
+                    },
+                },
+            ],
+        },
+    },
+};
 
 module.exports = serverlessConfiguration;
